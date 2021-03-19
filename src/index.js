cfcb0a9fe165f03b24798744d933378d1197c4d2
Elías De Vargas <sailedvc@gmail.com>
2021-03-19 17:19:47 -0500

    Add express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
 const { projects } = require('./data')
+
+/**
 const app = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'application/json'})
     response.end(JSON.stringify(projects))
 })
+ */
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hi from dev Portfolio v1.0.0</h1>')
+})
+
+app.get('/api/projects', (request, response) => {
+    response.json(projects)
+})
+
+app.get('/api/projects/:id', (request, response) => {
+    const { id } = request.params
+    const project = projects.find(p => p._id === id)
+    response.json(project)
+})
 
 const PORT = 3001
-app.listen(PORT)
-console.log('Server running on port ${PORT}')
+app.listen(PORT, () => {
+    console.log('Server started on port ${PORT}')
+})
