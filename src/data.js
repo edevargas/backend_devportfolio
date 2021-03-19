d1fcfb3ff58e3dfaf0ceb90b5b91d5cbddfb9b43
Elías De Vargas <sailedvc@gmail.com>
2021-03-19 16:58:23 -0500

    Create a fake json

---
+++ src/data.js
@@ -0,0 +1,36 @@
+module.exports = {
+    projects: [
+        {
+            _id: "kjg123",
+            name: "Project 1",
+            description: 'lorem',
+            categories: ['design', 'mobile'],
+            user: "111",
+            url: "http://google.com" 
+        },
+        {
+            _id: "fdf23",
+            name: "Project 2",
+            description: 'lorem',
+            categories: ['frontend', 'product design'],
+            user: "111",
+            url: "http://google.com" 
+        },
+        {
+            _id: "fsdfw234",
+            name: "Project 3",
+            description: 'lorem',
+            categories: ['frontend'],
+            user: "111",
+            url: "http://google.com" 
+        },
+        {
+            _id: "sdfsdf4",
+            name: "Project 4",
+            description: 'lorem',
+            categories: ['backend', 'nodejs'],
+            user: "111",
+            url: "http://google.com" 
+        }
+    ]
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const http = require('http')
-
+const { projects } = require('./data')
 const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain'})
-    response.end('Hi from portfolio')
+    response.writeHead(200, { 'Content-Type': 'application/json'})
+    response.end(JSON.stringify(projects))
 })
 
 const PORT = 3001
