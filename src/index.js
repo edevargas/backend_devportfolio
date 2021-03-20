44deabb16ceb3ea09b70097e995504643705fd07
Elías De Vargas <sailedvc@gmail.com>
2021-03-20 18:29:51 -0500

    CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const logger = require('./middlewares/loggerMiddleware')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(logger)
 
